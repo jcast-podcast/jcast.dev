e3268778266d145107b814ef47d1035af9c5d22f
Maarten Casteels <denmette@gmail.com>
2025-05-16 09:18:48 +0200

    chore: add fontawesome

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -114,7 +114,7 @@ resource "aws_cloudfront_response_headers_policy" "response_headers_policy" {
     content_security_policy {
       override = true
       # content_security_policy = "default-src 'self';"
-      content_security_policy = "default-src 'self'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; frame-src 'self' https://share.transistor.fm;"
+      content_security_policy = "default-src 'self'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; font-src 'self' https://cdnjs.cloudflare.com; frame-src 'self' https://share.transistor.fm;"
     }
 
     content_type_options {
