69b9545fce37aef9ac09f9a6b71324e058eb8a58
Maarten Casteels <denmette@gmail.com>
2025-06-02 08:55:38 +0200

    chore: remove tracked files that shouldn't be tracked

--- public/js/hamburger-menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.querySelector('.nav');
-
-    hamburger.addEventListener('click', function() {
-        nav.classList.toggle('open');
-        hamburger.classList.toggle('open');
-    });
-});
